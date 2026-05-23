55f9840f007984e6c6964e281fd6d66949e439d8
kyobankatu <kentokentokentokento601@gmail.com>
2026-05-23 16:18:28 +0000

    feat: add security header feat: change NextAuth session for 7 days

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -52,7 +52,10 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   ],
-  session: { strategy: 'jwt' },
+  session: {
+    strategy: 'jwt',
+    maxAge: 7 * 24 * 60 * 60,
+  },
   pages: { signIn: '/' },
   callbacks: {
     async jwt({ token, user }) {
